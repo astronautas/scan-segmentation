55bb6daa52caed89adceae9c9325f6364a0fe38c
Lukas Valatka <lukas@valatka.net>
2018-05-23 15:13:02 +0200

    Small additions

--- src/main/scala/shape_modelling/Utils.scala
+++ src/main/scala/shape_modelling/Utils.scala
@@ -5,12 +5,12 @@ import scalismo.mesh.TriangleMesh
 
 object Utils {
   def centerOfMass(points : Seq[Point[_3D]]) : Point[_3D] = {
-    var sum: Point[_3D] = new Point[_3D]
-
-    sum(0) = sum(0) / points.length
-    sum(1) = sum(0) / points.length
-    sum(2) = sum(0) / points.length
-
-    sum
+//    var sum: Point[_3D] = new Point[_3D]
+//
+//    sum(0) = sum(0) / points.length
+//    sum(1) = sum(0) / points.length
+//    sum(2) = sum(0) / points.length
+//
+//    sum
   }
 }
